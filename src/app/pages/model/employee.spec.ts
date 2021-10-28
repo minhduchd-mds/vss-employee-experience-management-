735d814cbf90dc4b61381f29a16c52fbf0f2f153
Minh Đức <ducdmd00410@fpt.edu.vn>
2021-10-28 11:33:31 +0700

    frgdfg
    fgd

--- src/app/pages/model/employee.spec.ts
+++ src/app/pages/model/employee.spec.ts
@@ -2,6 +2,7 @@ import { Employee } from './employee';
 
 describe('Employee', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new Employee()).toBeTruthy();
   });
 });
--- src/app/pages/model/tap.spec.ts
+++ src/app/pages/model/tap.spec.ts
@@ -2,6 +2,7 @@ import { Tap } from './tap';
 
 describe('Tap', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new Tap()).toBeTruthy();
   });
 });
